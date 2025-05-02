8f249108ca174366dd7a239d12835f59b081721f
parshadk <parshadk04@gmail.com>
2025-05-03 01:00:11 +0530

    added common package for zod and auth and updated devDependencies

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
 
+
 const app=express()
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("http server is running")
+})
 
 
 app.listen(3001,()=>{
