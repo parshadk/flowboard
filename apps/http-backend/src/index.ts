053c73848529060570819efc7e6bf499efc77597
parshadk <parshadk04@gmail.com>
2025-05-04 23:37:32 +0530

    added zod vaalidation in http app

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
-
+import {CreateUserSchema,CreateRoomSchema,SigninSchema } from "@repo/common/types"
 import { JWT_SECRET } from "@repo/common-backend/config";
 const app=express()
 
@@ -12,10 +12,23 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/signup",(req,res)=>{
-
+    const data=CreateUserSchema.safeParse(req.body)
+    if(!data.success){
+        res.status(400).json({
+            msg:"Invalid data",
+        })
+        return;
+    }
 })
 
 app.post("/signin",(req,res)=>{
+    const data=SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(400).json({
+            msg:"Invalid data",
+        })
+        return;
+    }
     const userId=1
     const token=jwt.sign({
         userId
@@ -23,7 +36,13 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/room",middleware,(req,res)=>{
-
+    const data=CreateRoomSchema.safeParse(req.body)
+    if(!data.success){
+        res.status(400).json({
+            msg:"Invalid data",
+        })
+        return;
+    }
 })
 
 app.listen(3001,()=>{
