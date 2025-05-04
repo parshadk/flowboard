a29c9fabd98f9459afd9db75a11fa1c5b89c8ba4
parshadk <parshadk04@gmail.com>
2025-05-04 23:26:47 +0530

    package updates and auth

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
 
-
+import { JWT_SECRET } from "@repo/common-backend/config";
 const app=express()
 
 app.use(express.json())
@@ -9,6 +11,20 @@ app.get("/",(req,res)=>{
     res.send("http server is running")
 })
 
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+    const userId=1
+    const token=jwt.sign({
+        userId
+    },JWT_SECRET)
+})
+
+app.post("/room",middleware,(req,res)=>{
+
+})
 
 app.listen(3001,()=>{
     console.log(`Server is running on port 3001`)
